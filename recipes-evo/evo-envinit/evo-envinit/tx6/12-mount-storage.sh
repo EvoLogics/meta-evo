93a9a0066fcf233f2c161a373f5a83cbed86f6d1
Victor Voronin <viktor.voronin@evologics.de>
2020-03-31 10:51:32 +0200

    recipes-evo/evo-envinit: add recipe for post-install target init scripts

---
+++ recipes-evo/evo-envinit/evo-envinit/tx6/12-mount-storage.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo -n "Checking if ssd exists... "
+if [ -b /dev/sda1 ]; then
+  echo -n "found, changing fstab... "
+  mkdir -p /mnt/storage
+  sed -i 's|#/dev/sda1|/dev/sda1|' /etc/fstab && echo "OK."
+  mount -a
+else
+  echo "not found!"
+
+  echo -n "Checking if sdcard exists... "
+  if [ -b /dev/mmcblk1p1 ]; then
+    echo -n "found, changing fstab... "
+    mkdir -p /mnt/storage
+    sed -i 's|#/dev/mmcblk1p1|/dev/mmcblk1p1|' /etc/fstab && echo "OK."
+    mount -a
+  else
+    echo "not found!"
+  fi
+fi
