3faa5eb427d0f5da409cc361c462049c3420d1a7
Victor Voronin <viktor.voronin@evologics.de>
2020-07-08 13:10:38 +0200

    recipes-evo/evo-envinit: also bind dune 'db' dir

--- recipes-evo/evo-envinit/evo-envinit/tx6/31-create-dune-dirs.sh
+++ recipes-evo/evo-envinit/evo-envinit/tx6/31-create-dune-dirs.sh
@@ -1,7 +1,8 @@
-#!/bin/bash
+#!/bin/sh
 
 DUNE_BASE="/opt/dune"
 DUNE_DIRS="db firmware lib log www"
+LINK_DIRS="db log"
 
 echo -n "Checking if dune exists... "
 if [ -e ${DUNE_BASE}/bin/dune ]; then
@@ -21,11 +22,18 @@ echo -n "Checking if storage mounted... "
 mount | grep -qi '/mnt/storage'
 if [ $? -eq 0 ]; then
   echo "found!"
-  mkdir -p /mnt/storage/dune-log
-  grep -iq '/mnt/storage/dune-log' /etc/fstab || cat >> /etc/fstab << EOF
 
-# uncomment this for dune-log bind
-/mnt/storage/dune-log   /opt/dune/log   none   defaults,bind   0   0
+  for dir in ${LINK_DIRS}; do
+    mkdir -p /mnt/storage/dune-${dir}
+
+    grep -iq "/mnt/storage/dune-${dir}" /etc/fstab || cat >> /etc/fstab << EOF
+
+# uncomment this for dune-${dir} bind
+/mnt/storage/dune-${dir}   /opt/dune/${dir}   none   defaults,bind   0   0
 EOF
+  done
+
   mount -a
+else
+  echo "not found!"
 fi
