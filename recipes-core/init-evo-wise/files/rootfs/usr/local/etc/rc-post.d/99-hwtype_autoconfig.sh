267e4c47f64a6aec3b976783d13a877e890ce9b0
Maksym Komar <komar@evologics.de>
2020-09-03 12:14:27 +0200

    Fix 99-hwtype_autoconfig.sh

--- recipes-core/init-evo-wise/files/rootfs/usr/local/etc/rc-post.d/99-hwtype_autoconfig.sh
+++ recipes-core/init-evo-wise/files/rootfs/usr/local/etc/rc-post.d/99-hwtype_autoconfig.sh
@@ -2,7 +2,8 @@
 
 case "$(evo-hw hwrev)" in
     r2-eth)
-        if evo-hw set-hw auto | logger -st init; then
+        if evo-hw set-hw auto; then
+            logger -st $0 change hardware type. need reboot
             MODE="$(awk '/ubi0:.*rootfs/ && sub(",.*","") {print $4}' < /proc/mounts)"
             mount -o remount,rw /
             chmod -x $0
