02943ead3ebf5707e503a8c9921336ba3f26d474
llewellyn-evo <llewellyn.fernandes@evologics.de>
2020-11-16 12:24:10 +0100

    update install scripts for error handling

--- recipes-support/swupdate-images-evo/swupdate-images-evo/commod-mx6ul/evo-swu-emb-system1.sh
+++ recipes-support/swupdate-images-evo/swupdate-images-evo/commod-mx6ul/evo-swu-emb-system1.sh
@@ -1,9 +1,20 @@
 #!/bin/sh
+check_command_success()
+{
+    if [ $1 -eq 0 ]; then
+        echo "Success"
+    else
+        exit 1
+    fi
+}
+
+
 do_preinst()
 {
     echo "do_preinst"
     #make ext4 file system
     mkfs.ext4 -F /dev/mmcblk1p4
+    check_command_success $?
     exit 0
 }
 
@@ -12,11 +23,15 @@ do_postinst()
     echo "do_postinst"
     echo "mounting Kernel and Device Tree"
     mount /dev/mmcblk1p3 /boot
+    check_command_success $?
     echo "Create Copy of device tree"
     cp /boot/imx6ul-comm-module.dtb /boot/oftree
+    check_command_success $?
     echo "Create Copy of kernel"
     cp /boot/zImage-mx6ul-comm-module.bin /boot/zImage
+    check_command_success $?
     umount /boot
+    check_command_success $?
     exit 0
 }
 
