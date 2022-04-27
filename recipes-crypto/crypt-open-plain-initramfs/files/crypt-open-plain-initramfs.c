d30740716545c46fea09108c5aa889a06dba75d3
Maksym Komar <komar@evologics.de>
2022-04-27 05:08:22 +0200

    Add crypt-open-plain-initramfs

---
+++ recipes-crypto/crypt-open-plain-initramfs/files/crypt-open-plain-initramfs.c
@@ -0,0 +1,38 @@
+#include <err.h>
+#include <stdio.h>
+#include <unistd.h>     /* geteuid() */
+#include <sys/types.h>
+#include <libcryptsetup.h>
+
+
+int main(int argc, char **argv)
+{
+	struct crypt_device *cd = NULL;
+    int rc;
+
+	if (geteuid())
+		errx(1, "Need root");
+
+	if (argc != 4)
+		errx(2, "Usage: crypt-open-plain-initramfs pass-file block-dev dm-name\n");
+
+    if ((rc = crypt_init(&cd, argv[2])))
+        errx(-rc, "crypt_init():");
+
+	rc = crypt_format(cd, CRYPT_PLAIN,
+			 "aes", "cbc-essiv:sha256",
+			 NULL, NULL,
+			 256 / 8,
+			 NULL);
+
+    if (rc < 0)
+        errx(-rc, "crypt_format():");
+
+    rc = crypt_activate_by_keyfile_device_offset(cd, argv[3],
+            CRYPT_ANY_SLOT, argv[1], 0, 0, 0);
+    if (rc < 0)
+        errx(-rc, "crypt_activate_by_keyfile():");
+
+	crypt_free(cd);
+    return 0;
+}
