24e2a077bedbd3cd58865205668227765d2a7793
llewellyn-evo <llewellyn.fernandes@evologics.de>
2022-09-19 16:18:06 +0200

    Add configuration for encrypted storage

--- recipes-evo/evo-envinit/evo-envinit/tegra194-evo/12-mount-storage.sh
+++ recipes-evo/evo-envinit/evo-envinit/tegra194-evo/12-mount-storage.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
 echo -n "Checking if sdcard exists... "
-if [ -b /dev/nvme0n1p1 ]; then
+
+if [ -b /dev/mapper/SSD ]; then
+  echo -n "found, changing fstab... "
+  mkdir -p /mnt/storage
+  sed -i 's|#/dev/mapper/SSD|/dev/mapper/SSD|' /etc/fstab && echo "OK."
+  mount -a
+elif [ -b /dev/nvme0n1p1 ]; then
   echo -n "found, changing fstab... "
   mkdir -p /mnt/storage
   sed -i 's|#/dev/nvme0n1p1|/dev/nvme0n1p1|' /etc/fstab && echo "OK."
