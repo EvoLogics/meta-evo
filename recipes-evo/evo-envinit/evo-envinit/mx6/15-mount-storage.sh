13fcc4d06e287b03ad62ed676bd42150bf53c403
Victor Voronin <viktor.voronin@evologics.de>
2023-10-30 13:45:20 +0100

    recipes-evo/evo-envinit: add to fstab not sed in 15-mount-storage.sh

--- recipes-evo/evo-envinit/evo-envinit/mx6/15-mount-storage.sh
+++ recipes-evo/evo-envinit/evo-envinit/mx6/15-mount-storage.sh
@@ -4,7 +4,11 @@ echo -n "Checking if ssd exists... "
 if [ -b /dev/sda1 ]; then
   echo -n "found, changing fstab... "
   mkdir -p /mnt/storage
-  sed -i 's|#/dev/sda1|/dev/sda1|' /etc/fstab && echo "OK."
+  cat >> /etc/fstab << EOF
+
+# uncomment this for sata ssd
+/dev/sda1            /mnt/storage          auto       defaults  0  0
+EOF
   mount -a
 else
   echo "not found!"
@@ -13,7 +17,11 @@ else
   if [ -b /dev/mmcblk1p1 ]; then
     echo -n "found, changing fstab... "
     mkdir -p /mnt/storage
-    sed -i 's|#/dev/mmcblk1p1|/dev/mmcblk1p1|' /etc/fstab && echo "OK."
+    cat >> /etc/fstab << EOF
+
+# uncomment this for sdcard
+/dev/mmcblk1p1       /mnt/storage          auto       defaults  0  0
+EOF
     mount -a
   else
     echo "not found!"
